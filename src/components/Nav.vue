2e4d70651acb42305c25320bf6d321b46ac7d971
Michealin White <mich.white@hotmail.com>
2018-07-10 18:06:05 +0100

    Added Navigatoin Bar

---
+++ src/components/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Nav Bar</h2>
+    <nav>
+      <router-link v-for="routes in links" 
+      v-bind:key="routes.id"
+      :to="`${routes.page}`">{{routes.text}}</router-link>
+    </nav>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Home',
+          page:'/Home'
+        },{
+          id: 1,
+          text: 'About',
+          page:'/ABout'
+        },{
+          id: 2,
+          text: 'Contact',
+          page:'/Contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style> 
+  .spacing { 
+    Margin-right: 10px; 
+  } 
+</style>
